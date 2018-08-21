9386db08374886ee4232b0d33765637e22ab0f16
Dominik Zieliński <dominias1988@gmail.com>
2018-08-21 13:46:27 +0200

    Initial commit

---
+++ src/main/java/pl/dezet/weddinghouse/WeddinghouseApplication.java
@@ -0,0 +1,12 @@
+package pl.dezet.weddinghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddinghouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddinghouseApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/dezet/weddinghouse/WeddinghouseApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.dezet.weddinghouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeddinghouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
